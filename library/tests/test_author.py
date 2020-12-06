62d77745b2baa1fa74a6181e465d0ba99bde1ebf
Hepsko <Adrian778@op.pl>
2020-12-06 17:28:57 +0100

    poprawa github actions

--- library/tests/test_author.py
+++ library/tests/test_author.py
@@ -1,17 +1,25 @@
 from django.test import TestCase
 from library.models import Author
 
+
 class AuthorTestCase(TestCase):
 
-    #Sprawdza czy dodanie identycznego akrota zwróci wyjątek
-    def  test_cant_add_an_existing_author(self):
+    def test_cant_add_an_existing_author(self):
+        """
+         Sprawdza czy dodanie identycznego akrota zwróci wyjątek
+        """
         Author.objects.create(name='Adam', surrname='Mickiewicz', nationality='Polska')
         self.assertRaises(Exception, Author.objects.create,name='Adam', surrname='Mickiewicz', nationality='Polska')
 
-    #Sporawdza czy powiedzie się próba dodania osoby bez imienia
     def test_cant_add_author_without_name(self):
+        """
+         Sporawdza czy powiedzie się próba dodania osoby bez imienia
+        """
         self.assertRaises(Exception, Author.objects.create, surrname="Sapkowski", nationality='Polska')
 
-    #Sporawdza czy powiedzie się próba dodania osoby bez imienia
+
     def test_cant_add_author_without_surrname(self):
+        """
+        Sporawdza czy powiedzie się próba dodania osoby bez imienia
+        """
         self.assertRaises(Exception, Author.objects.create, name="Andrzej", nationality='Polska')
