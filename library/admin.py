097f2be898afe4a0704177d0a88e207a57ac0b2b
MarcinDadura <dadura@student.agh.edu.pl>
2021-01-08 16:08:19 +0100

    Dodano klasy do panelu administratora

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Genre, Author, Book, BookCopy, Borrow, BorrowExtension
 
-# Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(BookCopy)
+admin.site.register(Borrow)
+admin.site.register(BorrowExtension)
